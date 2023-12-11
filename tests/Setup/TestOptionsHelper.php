ae27759b186ed903b6d1c3325036b1781f133d41
andrei <andrei@codemnky.nl>
2023-12-11 20:00:45 +0200

    removed update option since it is an wp function

--- tests/Setup/TestOptionsHelper.php
+++ tests/Setup/TestOptionsHelper.php
@@ -36,42 +36,4 @@ class TestOptionsHelper extends TestCase {
         $this->assertInstanceOf(OptionsHelper::class, $result);
     }
 
-    public function testInsertOption() {
-    // Arrange
-    $optionsHelper = new OptionsHelper();
-
-    // Mock the $_POST array
-    $_POST = ['nonce' => 'mocked_nonce', 'test_key' => 'test_value'];
-
-    // Mock the wp_verify_nonce function
-    Functions\expect('wp_verify_nonce')
-        ->once()
-        ->with('mocked_nonce', 'inpsyde_set_api')
-        ->andReturn(true);
-
-    // Mock the sanitize_text_field function
-    Functions\expect('sanitize_text_field')
-        ->once()
-        ->with('test_value')
-        ->andReturn('sanitized_value');
-
-    // Mock the update_option function
-    Functions\expect('update_option')
-        ->once()
-        ->with('test_key', 'sanitized_value')
-        ->andReturn(true);
-
-    // Mock the wp_unslash function
-    Functions\expect('wp_unslash')
-        ->once()
-        ->with('mocked_value')
-        ->andReturn('unslashed_value');
-
-    // Act
-    $result = $optionsHelper->insertOption('test_key');
-
-    // Assert
-    $this->assertTrue($result);
-    
-    }
 }
